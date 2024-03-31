863941957df052817c10780032c3b1c37f221b44
方法放寒假 <40489495+MiemieMethod@users.noreply.github.com>
2024-03-31 13:00:34 +0800

    [feat] 额外命令

--- command/builder.py
+++ command/builder.py
@@ -37,26 +37,28 @@ class CommandBuilder(MarkdownWriter):
             paramsWriter = MarkdownWriter()
             defList = {}
             description = '/{}'.format(self.command["name"])
+            print(self.command["name"], overload)
             for param in overload["params"]:
                 descriptorDict = self.enumsMap.get(param["type"]["name"], self.enums.get(param["type"]["name"], {}))
                 descriptor = descriptorDict.get('description', descriptorDict.get('name', param["type"]["name"]))
+                descriptor = 'int' if param["type"]["name"] == 'postfix_l' else descriptor
                 enumList = self.enums.get(descriptor.upper(), {}).get('values', None)
-                if enumList and len(enumList) == 1:
+                if isinstance(enumList, list) and len(enumList) == 1:
                     description += ' {}'.format(enumList[0]['value'])
                 else:
                     description += ' {}{}:{}{}{}'.format('[' if param["is_optional"] else '<', param["name"], descriptor, ']' if param["is_optional"] else '>', 'L' if param["type"]["name"] == 'postfix_l' else '')
 
                 dataType = '基本类型'
-                enumTable = MarkdownComponent()
                 enumTableText = ''
-                if enumList and len(enumList) >= 1:
+                if isinstance(enumList, list) and len(enumList) >= 1:
                     enumTable = MarkdownTable(['值', '描述'], [['`{}`'.format(e['value']), ''] for e in enumList])  # todo: add enum description
                     enumTableText = '枚举值如下：\n\n' + enumTable.render() if len(enumList) > 1 else '单值枚举，请直接使用`{}`。'.format(enumList[0]['value'])
                     dataType = '枚举类型'
-                elif enumList and len(enumList) == 0:
+                elif isinstance(enumList, list) and len(enumList) == 0:
                     dataType = '软枚举类型'
                 elif param["type"]["name"] == 'postfix_l':
-                    dataType = '后固定类型'
+                    enumTableText = '后缀为`L`。'
+                    dataType = '后缀类型'
                 defList['`{}`: {}'.format(param["name"], MarkdownSymbol('samp', descriptor).render())] = '{}。{}'.format(dataType, enumTableText)  # todo: add param description
             writer.addCodeBlock(description, 'mcfunction')
             writer.addHtmlBlock('div.result', MarkdownDefinitionList(defList, 5).render(), 4)
--- command/generator.py
+++ command/generator.py
@@ -9,17 +9,25 @@ class CommandGenerator(object):
         self.commands = {}
         self.enums = {}
         self.enumMaps = {}
+        self.commandList = {}
+        self.extraCommands = {}
         with open(r'assets/docs/command_modules/mojang-commands-internal.json', 'r') as f:
             self.commands = json.load(f)
         with open(r'assets/data/command_arg_types_full_v.json', 'r') as f:
             self.enumMaps = json.load(f)
         for enum in self.commands['command_enums']:
             self.enums[enum['name'].upper()] = enum
+        for command in self.commands['commands']:
+            self.commandList[command['name']] = command
+        with open(r'assets/extra/command/additional_commands.json', 'r') as f:
+            self.extraCommands = json.load(f)
+        self.mergeCommands()
+
 
     def generate(self):
         pageListText = ''
-        for c in self.commands['commands']:
-            command = c
+        for c in self.commandList:
+            command = self.commandList[c]
             builder = CommandBuilder(command, self.enums, self.enumMaps)
             if not os.path.exists(r'build/commands'):
                 os.mkdir(r'build/commands')
@@ -27,3 +35,27 @@ class CommandGenerator(object):
                 f.write(builder.render())
             pageListText += '- <code>{}</code>: refs/commands/{}.md\n'.format(command["name"], command["name"])
         return pageListText
+
+    def mergeCommands(self):
+        for e in self.extraCommands["command_enums"]:
+            if e["name"].upper() not in self.enums:
+                self.enums[e["name"].upper()] = e
+            else:
+                self.enums[e["name"].upper()]["values"].extend(e["values"])
+        for c in self.extraCommands["commands"]:
+            if c["name"] not in self.commandList:
+                self.commandList[c["name"]] = c
+            else:
+                overloadsDict = {}
+                for o in self.commandList[c["name"]]["overloads"]:
+                    overloadsDict[o["name"]] = o
+                for o in c["overloads"]:
+                    if o["name"] not in overloadsDict:
+                        overloadsDict[o["name"]] = o
+                    else:
+                        overloadsDict[o["name"]].update(o)
+                overloads = []
+                for o in overloadsDict:
+                    overloads.append(overloadsDict[o])
+                overloads.sort(key=lambda x: int(x["name"]))
+                self.commandList[c["name"]]["overloads"] = overloads
